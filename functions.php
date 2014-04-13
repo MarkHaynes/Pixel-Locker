516207cb06c8d1312d0a911f41b4ab8cbfd83f16
Mark Haynes <mark@markhaynes.me>
2014-04-13 13:42:24 +0100

    Functions.php & renaming
    Added better way to register menus in functions.php, now supports array
    of menus ideal for serving different menus.
    
    Renamed SASS files to use the best practise method.

--- functions.php
+++ functions.php
@@ -1,22 +1,14 @@
 <?php 
 
-// Registering first sidebar
-function widgets_init() {
-register_sidebar(array(
-'name' => __( 'Sidebar 1'),
-'id' => 'sidebar-1',
-'before_widget' => '',
-'after_widget' => '<br>',
-'before_title' => '<h4 class="side">',
-'after_title' => '</h4>',
-));
-}
-add_action( 'init', 'widgets_init' );
+	//Better way to register Menus
+	function register_menus() {
+		register_nav_menus(
+			array(
+				'header-menu' => __( 'Header Menu'),
+				'mobile-menu' => __( 'Mobile Menu'),
+				'footer-menu' => __( 'Footer Menu')
+		);
+	}
+	add_action ('init', 'register_menus');
 
-
-// Registering Custom Menu
-add_action('init','register_custom_menu');
-function register_custom_menu(){
-register_nav_menu('custom_menu',__('Custom Menu'));
-}
 ?>
