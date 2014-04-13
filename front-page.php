5dcc0b252b88e2f277fdeca18269b19128d9ce11
xLunchbox <lunchbox25@gmail.com>
2014-04-13 17:23:31 +0100

    Front Page Semantics

--- front-page.php
+++ front-page.php
@@ -11,69 +11,73 @@ Template Name: Custom Front Page
 	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
 	<main>
-		<div class="darkStrip">
-		
-			<h1>Welcome to <img id="pl-logo" alt="Pixel Locker Logo" src="<?php bloginfo('template_url'); ?>/images/pixellockerlogo.png" height="30px"></h1>
+		<section id="topStrip">
+			<div class="wrap">
 
-			<p>We are a friendly team that loves to design innovative and creative websites which comply with modern standards.</p>
+				<h1 class="pageTitle">Welcome to <img id="pl-logo" alt="Pixel Locker Logo" src="<?php bloginfo('template_url'); ?>/images/pixellockerlogo.png" height="30px"></h1>
+
+				<p>We are a friendly team that loves to design innovative and creative websites which comply with modern standards.</p>
+			
+			</div>
+		</section>
+
+		<section class="lightStrip">
+			<div class="wrap">
+
+				<h1>Always happy to help,</h1>
+
+				<p>We pride ourselves in our ability to provide all that you need to get started on the web. Our team has you covered...</p>
 	
-		</div>
+				<div class="servicesStrip">
 
-		<div class="lightStrip">
-		
-			<h2>Always happy to help,</h2>
-
-			<p>We pride ourselves in our ability to provide all that you need to get started on the web. Our team has you covered...</p>
-	
-			<div class="servicesStrip">
-				<div class="wrap">
-					<ul>
-						<li>
+					<ul id="servicesList">
+						<li class="serviceBox">
 							
 							<img class="serviceIcon" alt="Design Icon" src="<?php bloginfo('template_url'); ?>/images/wedesign.png" height="30px">
 
-							<h3>We Design</h3>
+							<h2 class="red">We Design</h2>
 
 							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
 							<p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
 
 						</li>		
-						<li>
+						<li class="serviceBox">
 							
 							<img class="serviceIcon" alt="Develop Icon" src="<?php bloginfo('template_url'); ?>/images/wedesign.png" height="30px">
 
-							<h3>We Develop</h3>
+							<h2 class="dev">We Develop</h2>
 
 							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
 							<p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
 
 						</li>		
-						<li>
+						<li class="serviceBox">
 							
 							<img class="serviceIcon" alt="Host Icon" src="<?php bloginfo('template_url'); ?>/images/wedesign.png" height="30px">
 
-							<h3>We Host</h3>
+							<h2 class="host">We Host</h2>
 
 							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
 							<p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
 
 						</li>
 					</ul>
+
 				</div>
-			</div>
 		
-			<h4>We have a lot to offer.</h4>
+				<h3>We have a lot to offer.</h3>
 
-			<div class="button">Read More</div>
-	
-		</div>
+				<div class="button">Read More</div>
 
-		<div class="whiteStrip">
+			</div>
+		</section>
+
+		<section class="whiteStrip">
 			<div class="wrap">
 			
 				<div class="meetTheTeam">
 
-					<h2>Meet The Team</h2>
+					<h1>Meet The Team</h1>
 
 					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
 
@@ -83,11 +87,11 @@ Template Name: Custom Front Page
 
 				</div>
 
-				<ul class="teamMembers">
+				<ul id="teamMembers">
 					<li>
 						<img class="avatar" alt="Photo Of Mark" src="<?php bloginfo('template_url'); ?>/images/mark.png" height="30px">
 
-						<h4>Mark Haynes</h4>
+						<h2>Mark Haynes</h2>
 
 						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
 
@@ -98,7 +102,7 @@ Template Name: Custom Front Page
 					<li>
 						<img class="avatar" alt="Photo Of Chris" src="<?php bloginfo('template_url'); ?>/images/mark.png" height="30px">
 
-						<h4>Chris Huxley</h4>
+						<h2>Chris Huxley</h2>
 
 						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
 
@@ -108,16 +112,7 @@ Template Name: Custom Front Page
 				</ul>
 
 			</div>		
-		</div>
-
-		<div class="darkStrip">
-		
-			<h4>It would be great to hear from you :)</h4>
-
-			<div class="button"><a href="http://pixellocker.co.uk">Contact Us</a></div>
-	
-		</div>
-
+		</section>
 	</main>
 	
    		
