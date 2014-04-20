22ecc125caf77b5caedd13e316d57df984a434f4
xLunchbox <lunchbox25@gmail.com>
2014-04-21 00:20:28 +0100

    A couple of changes
    Changes made to single.php, front-page.php and made some styling
    adjustmensts.

--- single.php
+++ single.php
@@ -2,31 +2,37 @@
 <?php get_header(); ?>
 
 	<main>
+
+		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
 		<section class="lightStrip">
 			<div class="wrap">
-				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+				
 				
 				<h1 class="title"><?php the_title(); ?></h1>
 
-				<span class="meta"><?php the_time( 'D, jS F Y' ); ?> / / Posted in <?php the_category(', '); ?></span>
+				<span class="postInfo"><?php the_time( 'D, jS F Y' ); ?> / / Posted in <?php the_category(', '); ?></span>
 			
 			</div>
 		</section>
 
 		<section class="whiteStrip">
+
 			<?php if ( has_post_thumbnail() ) { the_post_thumbnail(); } ?>
-				<div class="post">
-		   		
-			   		<?php the_content(); ?>
 
-					<?php endwhile; else: ?>
+			<div class="post">
+	   		
+		   		<?php the_content(); ?>
 
-			 			<p>Sorry, no posts matched your criteria.</p>
+				<?php endwhile; else: ?>
+					
+		 			<h1>Sorry, no posts matched your criteria.</h1>
 
-			 		<?php endif; ?>
+				<?php endif; ?>
 
-		 		</div>
+	 		</div>
 	 	</section>
+
  	</main>
 
 <?php get_footer(); ?>
