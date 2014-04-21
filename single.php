3e6124eef9d78ba4052740396487aad565234efe
xLunchbox <lunchbox25@gmail.com>
2014-04-21 05:47:33 +0100

    Finished posts

--- single.php
+++ single.php
@@ -5,37 +5,46 @@
 
 		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-		<section class="lightStrip">
-			<div class="wrap">
+		<article class="post">
+			<section class="lightStrip">
+				<div class="wrap">
+					
+					
+					<h1 class="title"><?php the_title(); ?></h1>
+
+					<span class="postInfo"><?php the_time( 'D, jS F Y' ); ?> / / Posted in <?php the_category(', '); ?></span>
 				
-				
-				<h1 class="title"><?php the_title(); ?></h1>
+				</div>
+			</section>
 
-				<span class="postInfo"><?php the_time( 'D, jS F Y' ); ?> / / Posted in <?php the_category(', '); ?></span>
-			
-			</div>
-		</section>
+			<section class="whiteStrip">
+				<div class="postWrap">
 
-		<section class="whiteStrip">
+					<div class="thumb"></div>
 
-			<?php if ( has_post_thumbnail() ): ?>
-				<div class="thumb">
-                    <a href="<?php the_permalink() ?>"><?php the_post_thumbnail(); ?></a>
-                </div>
-            <?php  endif; ?>
+					<?php if ( has_post_thumbnail() ): ?>
+						<div class="thumb">
+		                    <a href="<?php the_permalink() ?>"><?php the_post_thumbnail(); ?></a>
+		                </div>
+		            <?php  endif; ?>
+		   		
+			   		<?php the_content(); ?>
 
-			<div class="post">
-	   		
-		   		<?php the_content(); ?>
+		 		</div>
+		 	</section>
+		</article>
 
-				<?php endwhile; else: ?>
+		<?php endwhile; else: ?>
+        
+			<section class="notFound">
 
-		 			<h1>Sorry, no posts matched your criteria.</h1>
+				<h1>Sorry, we could not find what you are looking for.</h1>
 
-				<?php endif; ?>
+	            <div class="btn"><a href="<?php echo get_option('home'); ?>">Return to the homepage</a></div>
 
-	 		</div>
-	 	</section>
+		 	</section>
+            
+        <?php endif; ?>
 
  	</main>
 
