1beb83515efe5acfe5e50d036187dff883eb035d
xLunchbox <lunchbox25@gmail.com>
2014-04-23 09:50:06 +0100

    Services mocked up
    Also made a few other changes mainly dimensions.

--- services.php
+++ services.php
@@ -19,11 +19,57 @@ Template Name: Services Page
 			</div>
 		</section>
 
-		<section class="whiteStrip">
+		<section id="design">
 			<div class="wrap">
-					
-				<h1 class="title"><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h1>
+				<div class="serviceDesc">	
+					<h1><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h1>
 
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+
+					<p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+
+					<p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+
+					<div class="serviceBtn"><a href="<?php the_permalink() ?>">Find out more...</a></div>
+
+				</div>
+				<div class="clear"></div>
+			</div>
+		</section>
+
+		<section id="develop">
+			<div class="wrap">
+				<div class="serviceDesc">	
+					<h1><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h1>
+
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+
+					<p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+
+					<p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+
+					<div class="serviceBtn"><a href="<?php the_permalink() ?>">Find out more...</a></div>
+
+				</div>
+				<div class="clear"></div>
+			</div>
+		</section>
+
+		<section id="host">
+			<div class="wrap">
+				<div class="serviceDesc">	
+					<h1><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h1>
+
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+
+					<p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+
+					<p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+
+					<div class="serviceBtn"><a href="<?php the_permalink() ?>">Find out more...</a></div>
+
+				</div>
+				<div class="clear"></div>
 			</div>
 		</section>
 	</main>
