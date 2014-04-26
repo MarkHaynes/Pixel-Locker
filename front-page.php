4637b19f6f97e2a71c53dfa9b7f04abe6a1e23f7
xLunchbox <lunchbox25@gmail.com>
2014-04-26 17:47:14 +0100

    Changed the meet the team section
    Changes applied to desktop tablet and mobile.

--- front-page.php
+++ front-page.php
@@ -76,13 +76,6 @@ Template Name: Custom Front Page
 				<div id="meetTheTeam">
 					<div id="teamDesc">
 						<h1>Meet The Team</h1>
-
-						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-
-						<p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-
-						<p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-
 					</div><!--
 					--><div id="teamMembers">
 						<div class="teamMember">
